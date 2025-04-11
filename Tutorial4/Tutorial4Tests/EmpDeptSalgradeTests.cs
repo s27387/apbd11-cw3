16a8a62898b1f61551aa8bafdf8285c7a67d5872
Rukuretia <rukuretio@gmail.com>
2025-04-11 12:17:07 +0200

    1-4 EMP

--- Tutorial4/Tutorial4Tests/EmpDeptSalgradeTests.cs
+++ Tutorial4/Tutorial4Tests/EmpDeptSalgradeTests.cs
@@ -9,7 +9,7 @@ public class EmpDeptSalgradeTests
     {
         var emps = Database.GetEmps();
 
-        List<Emp> result = null;
+        List<Emp> result = emps.Where(e => e.Job == "SALESMAN").ToList();;
 
         Assert.Equal(2, result.Count);
         Assert.All(result, e => Assert.Equal("SALESMAN", e.Job));
@@ -22,7 +22,10 @@ public class EmpDeptSalgradeTests
     {
         var emps = Database.GetEmps();
 
-        List<Emp> result = null; 
+         List<Emp> result = emps
+        .Where(e => e.DeptNo == 30)
+        .OrderByDescending(e => e.Sal)
+        .ToList();
 
         Assert.Equal(2, result.Count);
         Assert.True(result[0].Sal >= result[1].Sal);
@@ -36,7 +39,12 @@ public class EmpDeptSalgradeTests
         var emps = Database.GetEmps();
         var depts = Database.GetDepts();
 
-        List<Emp> result = null; 
+        List<Emp> result = emps
+        .Where(e => depts
+            .Where(d => d.Loc == "CHICAGO")
+            .Select(d => d.DeptNo)
+            .Contains(e.DeptNo))
+        .ToList();
 
         Assert.All(result, e => Assert.Equal(30, e.DeptNo));
     }
@@ -48,13 +56,15 @@ public class EmpDeptSalgradeTests
     {
         var emps = Database.GetEmps();
 
-        //var result = null; 
+        var result = emps
+        .Select(e => new { e.EName, e.Sal })
+        .ToList(); 
         
-        // Assert.All(result, r =>
-        // {
-        //     Assert.False(string.IsNullOrWhiteSpace(r.EName));
-        //     Assert.True(r.Sal > 0);
-        // });
+        Assert.All(result, r =>
+        {
+            Assert.False(string.IsNullOrWhiteSpace(r.EName));
+            Assert.True(r.Sal > 0);
+        });
     }
 
     // 5. JOIN Emp to Dept
